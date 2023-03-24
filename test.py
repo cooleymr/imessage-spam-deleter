f0c7be29b5fd4b81aa5ab96d60c748a755a0387b
Mac Cooley <mrcooley@iu.edu>
2023-03-23 20:55:36 -0400

    improved gui and added date and name info to mess

--- test.py
+++ test.py
@@ -1,34 +1,25 @@
-# from imessage_reader import fetch_data
+from imessage_reader import fetch_data
 import datetime
 import sqlite3
 from sqlite3 import Error
-import fetch_data
-
 
 # -m pip install mysql-connector-python
 
+# Message object contains (in tuple order): 
+# [0]: user id (sender's or recipient's phone number or email address)
+# [1]: text message (String)
+# [2]: date and time
+# [3]: service (iMessage or SMS)
+# [4]: account (destination caller id)
+# [5]: is the message from me
 
-#Convertstring to date time object
+def line():
+    print("\n")
+    print("...")
+    print("\n")
 
-# for message in messages:
-#     senderNumber = message[0]
-#     date = datetime.datetime.strptime(message[2].split(' ')[0], '%Y-%m-%d')
-#     text = message[1]
-#     service = message[3]
-#     isFromMe = message[5]
-
-#     if date > date_threshold:
-#         if text is not None:
-#             if str(service) == "SMS":
-#                 if isFromMe == 0:
-#                     print(text)
-
-## FUNCTIONS TO DELETE MESSAGES FROM DATABASE
 def create_connection(db_file):
-    # create a database connection to the SQLite database
-    # specified by the db_file
-    # :param db_file: database file
-    # :return: Connection object or None
+    line()
     conn = None
     try:
         conn = sqlite3.connect(db_file)
@@ -37,36 +28,37 @@ def create_connection(db_file):
         print(e)
     return conn
 
+# Prompts user if they want to delete this message. Continues until user answers Y,N, or STOP. Returns answer as String.
 def promptUser(message):
-    # Prompts the user if they want to delete this message
-    # :message: text message to be prompted
-    # :return: true if user wants to delete message, false otherwise
-    # print("\n")
+    line()
+    print(message[1])
     print("\n")
-    print(message)
-    print("Are you sure you want to delete this message?:  (Y/N)")
+    print("Sent On: " + message[2]) 
+    print("From: " + message[0]) 
+    print("\n")
+    print("Are you sure you want to delete all messages from this address?:  (y/n)")
     answer = input()
-    while(answer != "Y" and answer != "N" and answer != "STOP"):
-        print("Please enter Y or N or STOP to end program")
+    while(answer != "y" and answer != "n" and answer != "STOP"):
+        print("Please enter y or n or STOP to end program")
         answer = input()
     return answer
 
+
+# Deletes tasks by ID. 
 def delete_task(conn, id):
-    # Delete a task by task id
-    # :param conn:  Connection to the SQLite database
-    # :param id: id of the task
-    # :return: none
-
+    task = "DELETE FROM main.chat WHERE guid LIKE ?;"
     cur = conn.cursor()
-
-    temp = "DELETE FROM main.chat WHERE guid LIKE ?;"
-
-    
-    cur.execute(temp, ('%' + id + '%',))    
+    cur.execute(task, ('%' + id + '%',))    
     conn.commit()
-    print ("Total number of rows deleted :" + str(conn.total_changes))
 
-## FUNCTIONS TO GET ONLY SPAM MESSAGES
+    print("Message Deleted")
+
+    # print number of rows deleted 
+    global totalChanges
+    print ("Messages Deleted: " + str(conn.total_changes - totalChanges))
+    totalChanges = conn.total_changes
+
+# Gets messages that are from a unique number. (A number that only has ever sent you 1 message ever. Ex. NOT Venmo)
 def get_unique_number_tuples(messages):
     number_count = {}
     for message in messages:
@@ -78,10 +70,22 @@ def get_unique_number_tuples(messages):
     unique_numbers = [message for message in messages if number_count[message[0]] == 1]
     return unique_numbers
 
+# Gets number of addresses(senders) in list
+def get_number_addresses(messages):
+    number_count = {}
+    for message in messages:
+        number = message[0]
+        if number not in number_count:
+            number_count[number] = 1
+        else:
+            number_count[number] += 1
+    unique_numbers = [message for message in messages if number_count[message[0]] == 1]
+    return len(unique_numbers)
+
 # Returns a list of message objects that contain spam messages in the last 30 days
 def get_spam_messages():
     fd = fetch_data.FetchData()
-    messages = fd.get_messages() #Getting all messages in chat.db file on Mac
+    messages = fd.get_messages() #Getting all messages in chat.db file on Mac. Fetch Data takes from main chat.db file, not test.
 
     #Filtering to only messages in the last 30 days
     date_threshold = datetime.datetime.now() - datetime.timedelta(days=30)
@@ -99,38 +103,29 @@ def get_spam_messages():
     return messages
 
 def main():
-    # database = r"/Users/maccooley/Desktop/chat.db" # Path to test file
+    global totalChanges
+    totalChanges = 0
+
+    # database = r"/Users/maccooley/Desktop/copy chat.db" # Path to test chat.db file
     database = r"/Users/maccooley/Library/Messages/chat.db" # Path to chat.db file 
     
-    # Getting list of spam messages
-    spamMessages = get_spam_messages()
-
-    for temp in spamMessages:
-        print("\n" + str(temp))
-
     # create a database connection
     conn = create_connection(database)
     def after_delete_message_plugin():
         print("message deleted")
-    conn.create_function("after_delete_message_plugin", -1, after_delete_message_plugin)
+    conn.create_function("after_delete_message_plugin", -1, after_delete_message_plugin) # Creating missing after_delete_message function. Program will stop without this.
 
-    
+    # Getting list of spam messages
+    spamMessages = get_spam_messages()
+
+    # Running main program
+    print("\nFound: " + str(len(spamMessages)) + " spam messages from " + str(get_number_addresses(spamMessages)) + " addresses")
     for message in spamMessages:
-        userAnswer = promptUser(message[1])
-        if userAnswer== "Y":
+        userAnswer = promptUser(message)
+        if userAnswer== "y":
             delete_task(conn, message[0])
         elif userAnswer == "STOP":
             break
 
-
 if __name__ == '__main__':
     main()
-
-
-# Message object contains (in tuple order): 
-# user id (sender's or recipient's phone number or email address)
-# message
-# date and time
-# service (iMessage or SMS)
-# account (destination caller id)
-# is the message from me
